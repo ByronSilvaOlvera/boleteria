d23118bace8eb6a4b40724663953a9f2640064ea
Byron Silva Olvera <byronsjimb@gmail.com>
2021-05-03 15:35:45 -0500

    creacion de model Menu

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    icon       = models.CharField(max_length=50)
+    rutaName   = models.CharField(max_length=50) 
+    nombre     = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.nombre
+
+    def icon_get(self):
+        return self.icon
--- boleteria/settings.py
+++ boleteria/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'apps.dashboard',
+    'apps.dashboard',
 ]
 
 MIDDLEWARE = [
