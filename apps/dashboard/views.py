ed1c2f5207388f55fd42bbf64e908362a0ba3c15
Byron Silva Olvera <byronsjimb@gmail.com>
2021-05-03 15:57:58 -0500

    vista en dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("mi dashboard")
 
+def inicioSeccion(request , id_sec)
+    return HttpResponse('Seccion %s.' % id_sec )    
+
 # Create your views here.
