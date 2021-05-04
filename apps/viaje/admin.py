cd624c0addf63c94a0ba1cac13b451c4ca9a81f4
Byron Silva Olvera <byronsjimb@gmail.com>
2021-05-03 21:32:56 -0500

    menu

--- apps/viaje/admin.py
+++ apps/viaje/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Cajero
+from .models import Cajero, Boleto
 
 admin.site.register(Cajero)
+admin.site.register(Boleto)
